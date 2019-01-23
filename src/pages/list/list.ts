1a11aaa55f54950c8383179e7c7d0c3cb0c6f63f
NISHI3 <18317954+NISHI3@users.noreply.github.com>
2019-01-23 21:36:45 +0900

    Listpage修正

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -9,27 +9,9 @@ import { ListinfoPage } from '../listinfo/listinfo';
   templateUrl: 'list.html'
 })
 export class ListPage {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
   bles: Array<{area_name: string, ble_uuid: string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public api: ApiProvider) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
     api.getBleList()
       .subscribe(data => {
         this.bles = data['result'];
@@ -43,12 +25,5 @@ export class ListPage {
     this.navCtrl.push(ListinfoPage, {
       ble: ble
     })
-  }
-
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
-      item: item
-    });
   }
 }
