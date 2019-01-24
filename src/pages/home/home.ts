6b080b5d5930c6e3803bc8876e76bc818aa9f271
NISHI3 <18317954+NISHI3@users.noreply.github.com>
2019-01-25 03:02:46 +0900

    遷移button追加

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,14 +3,17 @@ import { NavController } from 'ionic-angular';
 import { ApiProvider } from '../../providers/api/api';
 import { Ble } from '../../providers/interfaces/Ble';
 
+import { CreateLetterPage } from '../create-letter/create-letter'
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
   bles: Ble[];
+  pushPage: any;
   constructor(public navCtrl: NavController, public api: ApiProvider) {
-    
+    this.pushPage = CreateLetterPage;
   }
   getData() {
     this.api.getBleList()
