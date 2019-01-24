fb478dcda35b39dd2802b74627c9a0230d53c014
NISHI3 <18317954+NISHI3@users.noreply.github.com>
2019-01-25 03:02:05 +0900

    CreateLetterページ追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 import { ListinfoPage } from '../pages/listinfo/listinfo';
+import { CreateLetterPage } from '../pages/create-letter/create-letter';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -19,7 +20,8 @@ import { ApiProvider } from '../providers/api/api';
     MyApp,
     HomePage,
     ListPage,
-    ListinfoPage
+    ListinfoPage,
+    CreateLetterPage
   ],
   imports: [
     BrowserModule,
@@ -32,7 +34,8 @@ import { ApiProvider } from '../providers/api/api';
     MyApp,
     HomePage,
     ListPage,
-    ListinfoPage
+    ListinfoPage,
+    CreateLetterPage
   ],
   providers: [
     StatusBar,
