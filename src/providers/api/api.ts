7577b993abb36083005f8c735032daa779d71555
NISHI3 <18317954+NISHI3@users.noreply.github.com>
2019-01-25 02:06:32 +0900

    basic認証対応

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
@@ -13,15 +13,26 @@ import { Ble } from '../interfaces/Ble'
 */
 @Injectable()
 export class ApiProvider {
-
+  //　APIのURLを変更
+  url = 'https://kakioku-api-server.herokuapp.com';
+  // btoaでbase64エンコードしているので、'user:password'で変換
+  basic = btoa('user:password')
+  headers = new HttpHeaders().set('Authorization', `Basic ${this.basic}`);
   constructor(public http: HttpClient) {
     
   }
   getBleList(): Observable<any> {
-    return this.http.get<Ble[]>('/api/ble/get');
+    return this.http.get<Ble[]>(`${this.url}/api/ble/get`, {
+      headers : this.headers
+    });
   }
 
   getMessage(): Observable<any> {
     return this.http.get('/api/message/get');
   }
 }
+
+
+// Basic dXNlcjpwYXNzd29yZA==
+// headers: new HttpHeaders().set('Authorization', 'basic aGM0YXBwOmHjddP5NjU='),
+// Basic dXNlcjpwYXNzd29yZA==
