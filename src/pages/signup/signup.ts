3166c8b202f66c08d7b4f653bdd0f3fd9b92f123
NISHI3 <18317954+NISHI3@users.noreply.github.com>
2019-01-26 17:39:03 +0900

    sighinページ追加

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ApiProvider } from '../../providers/api/api';
+import { HomePage } from '../home/home';
+import { UUID } from 'angular2-uuid';
 
 /**
  * Generated class for the SignupPage page.
@@ -14,12 +17,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'signup.html',
 })
 export class SignupPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  inputName: string = '';
+  uuid: string = '';
+  errorMessage: string = '';
+  constructor(public navCtrl: NavController, public navParams: NavParams, public api: ApiProvider) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SignupPage');
+  submitButton() {
+    this.api.sighup(this.inputName, UUID.UUID())
+      .subscribe(
+        data => {
+          if(data.code === 200) {
+            localStorage.setItem("sighinUser", this.inputName);
+            this.navCtrl.setRoot(HomePage);
+          }
+          else if(data.code === 409) {
+            this.errorMessage = "なまえががぶってるぉ";
+          }
+        }, err => console.log(err),
+        () => {}
+      )
   }
 
 }
